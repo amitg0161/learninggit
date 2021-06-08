46bbf2a733e749d138ae77386e500bf631ad49b9
Amit Gupta <amitg0161@gmail.com>
2021-06-08 17:25:32 +0000

    commiting changes

--- routes.py
+++ routes.py
@@ -11,6 +11,9 @@ def index():
 @app.route('/symbol.html')
 def symbol():
     return render_template('symbol.html', the_title='Tiger As Symbol')
+    
+def unknown():
+    return render_template('symbol.html', the_title='Tiger As Symbol')
 
 @app.route('/myth.html')
 def myth():
